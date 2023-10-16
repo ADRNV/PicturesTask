7376d9239dee2a3647d9dd8782f66b536fb759ed
ADRN_NKT <combinerk@gmail.com>
2023-10-16 12:36:34 +0300

    Add Core models

---
+++ PicturesTask.Core/Models/Friend.cs
@@ -0,0 +1,11 @@
+﻿namespace PicturesTask.Core.Models
+{
+    public class Friend
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid FriedId { get; set; }
+    }
+}
